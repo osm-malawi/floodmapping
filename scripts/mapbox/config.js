0fc2da78b16ece93fda9b80fe350b6ca9859104e
Patrick Ken Kalonde <39599487+Kalondepatrick@users.noreply.github.com>
2023-05-19 08:56:03 +0200

    changed directory structure

--- scripts/mapbox/config.js
+++ scripts/mapbox/config.js
@@ -11,12 +11,133 @@
     use3dTerrain: false, //set true for enabling 3D maps.
     title: 'ESA Hub Malawi Map Story',
     subtitle: 'A story about FDGs that were conducted in Nsanje and Chikwawa',
-    byline: 'By Esu hub Malawi',
-    footer: 'Wonder just trying out tings nah mean.',
+    byline: 'By ESA hub Malawi',
+    footer: 'Malawi Red Cross Society - Nkhoma University - OSM Malawi.',
     chapters: [
         {
-            id: 'Chikwawa',
+            id: 'Lilongwe',
             alignment: 'left',
+            hidden: false,
+            title: 'Lilongwe University of Agriculture and NAtural Resources',
+            image: 'images/luana.jpg',
+            description:'The first flooding in this particular community, there is no designated evacuation centre. However, during this flooding, affected communities settled/evacuated to one of  the nearby churches which is located on higher ground.They reported that the most notable challenges are Low food supplies/support, Marriage/family disconnection.',
+            location: {
+                center: [33.770254, -14.183325],
+                zoom: 10.4,
+				pitch: 59.5,
+				bearing: -1.19
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1,
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1
+                 }
+            ],
+        },
+
+        {
+            id: 'Nkhoma',
+            alignment: 'right',
+            hidden: false,
+            title: 'Nkhoma University',
+            image: 'images/Nkhoma.jpg',
+            description:'The first flooding in this particular community, there is no designated evacuation centre. However, during this flooding, affected communities settled/evacuated to one of  the nearby churches which is located on higher ground.They reported that the most notable challenges are Low food supplies/support, Marriage/family disconnection.',
+            location: {
+                center: [34.10626259672374, -14.037872304743676], 
+                zoom: 10.4,
+				pitch: 59.5,
+				bearing: -1.19
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1,
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1
+                 }
+            ]
+        },
+
+        {
+            id: 'Chanco',
+            alignment: 'left',
+            hidden: false,
+            title: 'Chanco University',
+            image: 'images/Chanco.jpg',
+            description:'The first flooding in this particular community, there is no designated evacuation centre. However, during this flooding, affected communities settled/evacuated to one of  the nearby churches which is located on higher ground.They reported that the most notable challenges are Low food supplies/support, Marriage/family disconnection.',
+            location: {
+                center: [35.33728019287816, -15.389368271134405],
+                zoom: 10.4,
+				pitch: 59.5,
+				bearing: -1.19
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1,
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1
+                 }
+            ]
+        },
+
+        {
+            id: 'Must',
+            alignment: 'left',
+            hidden: false,
+            title: 'Malawi University of Science and Technology',
+            image: 'images/Must.jpg',
+            description:'The first flooding in this particular community, there is no designated evacuation centre. However, during this flooding, affected communities settled/evacuated to one of  the nearby churches which is located on higher ground.They reported that the most notable challenges are Low food supplies/support, Marriage/family disconnection.',
+            location: {
+                center: [35.21690014084476, -15.90174986456486],
+                zoom: 10.4,
+				pitch: 59.5,
+				bearing: -1.19
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1,
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'natmall-visitors-circles',
+                     opacity: 1
+                 }
+            ]
+        },
+
+
+        {
+            id: 'Chikwawa',
+            alignment: 'right',
             hidden: false,
             title: 'Chikwawa',
             image: 'images/Dronepic1.jpg',
@@ -24,7 +145,7 @@
             location: {
                 center: [34.760559, -16.062261],
                 zoom: 10.4,
-				pitch: 5.5,
+				pitch: 59.5,
 				bearing: -1.19
             },
             mapAnimation: 'flyTo',
@@ -45,7 +166,7 @@
         },
         {
             id: 'Nsanje',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
             title: 'Nsanje',
             image: 'images/Dronepic2.jpg',
